65919e2ee2565c9436c31d3a2d1351377e32d261
Karim7651 <karimatef785@gmail.com>
2025-04-26 21:20:54 +0300

    test

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const DB = process.env.DATABASE.replace(
 mongoose.connect(DB).then(() => console.log("DB connection successful!"));
 
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080 ;
 console.log(process.env.NODE_ENV);
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
